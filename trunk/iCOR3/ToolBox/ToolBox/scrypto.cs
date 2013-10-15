5d0cd842074b38d5798f2ed98071b6a73124af49
Gregor.Spyra@googlemail.com <Gregor.Spyra@googlemail.com@099c87c5-d013-02bb-3cc7-7b4dfb8cf8b7>
2013-10-15 12:16:19 +0000

    CHANGE: Added Command Line ToolBox - scrypto.exe; Encrypts/Decrypts plain text input

---
+++ trunk/iCOR3/ToolBox/ToolBox/scrypto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using iCOR3.iSecurityComponent;
+
+namespace ToolBox
+{
+	class scrypto
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				Message();
+			}
+			else
+			{
+				try
+				{
+					if (args[0].Equals("/E", StringComparison.OrdinalIgnoreCase))
+					{
+						Console.WriteLine(iCOR3.iSecurityComponent.SimpleCrypto.EncryptString(args[1]));
+					}
+					else if (args[0].Equals("/D", StringComparison.OrdinalIgnoreCase))
+					{
+						Console.WriteLine(iCOR3.iSecurityComponent.SimpleCrypto.DecryptString(args[1]));
+					}
+					else
+					{
+						Message();
+					}
+				}
+				catch (Exception eX)
+				{
+					Console.WriteLine("Error: Incorrect input string format!");
+				}
+			}
+		}
+
+		static void Message()
+		{
+			Console.WriteLine(String.Format(@"
+Please use:
+{0} [/E] [/D] ""plain text""
+	/E	Encrypt to Base64 format
+	/D	Decrypt from Base64 format
+",
+		   Path.GetFileName(Assembly.GetExecutingAssembly().CodeBase)
+		   ));
+		}
+	}
+}
